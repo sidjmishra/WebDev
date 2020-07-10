9622b826830903148df0ec917f3af470d470261a
Siddhant Mishra <sidjmishra007@gmail.com>
2020-07-11 02:57:36 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -49,15 +49,18 @@ function division(a, b) {
 }
 
 // Form Validation
-function formValidate(a, b) {
+function formValidate(a) {
 	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	const decimal =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 	if(re.test(a)) {
 		document.getElementById('validate1').innerHTML = "";
 	} else {
 		document.getElementById('validate1').innerHTML = "Please Enter Valid EmailId!!";
 	}
-	if(decimal.test(b)) {
+}
+
+function formPassword(a) {
+	const decimal =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+	if(decimal.test(a)) {
 		document.getElementById('validate2').innerHTML = "";
 	} else {
 		document.getElementById('validate2').innerHTML = "Password should contain atleast 1 Upper, 1 Lower, 1 Special, 1 Numeric and length 8-15 chars";
